8bdfa07ba7d226cf6953bd4013f3b790a6ee36d9
anvi-git <antonio.viscusi@edu.unito.it>
2026-05-28 19:22:50 +0200

    new

--- notebooks/blast_code/src/new_funcs.jl
+++ notebooks/blast_code/src/new_funcs.jl
@@ -1,56 +1,148 @@
-function bessel_cheb_eval(ℓ::Number, chi::AbstractArray, zed::AbstractArray, kmin::Number, kmax::Number, zmin::Number, zmax::Number; n_cheb::Int, N::Number)
+# Definisci una volta sola fuori dai loop
+struct IntegrationGrid
+    z::Vector{Float64}
+    w::Vector{Float64}
+    jac::Float64
+end
 
+# Inizializza una volta sola
+function setup_grid(zmin, zmax, N)
+    CC_obj = FastTransforms.chebyshevmoments1(Float64, N)
+    t_cc   = FastTransforms.clenshawcurtisnodes(Float64, N)
+    w_cc   = FastTransforms.clenshawcurtisweights(CC_obj)
+    z_cc   = @. (zmax + zmin) / 2 + (zmax - zmin) / 2 * t_cc
+    return IntegrationGrid(z_cc, w_cc, (zmax - zmin) / 2)
+end
+
+# function bessel_cheb_eval(ℓ::Number, chi::AbstractArray, zed::AbstractArray, kmin::Number, kmax::Number, zmin::Number, zmax::Number; n_cheb::Int, N::Number)
+
+#     chi_z = DataInterpolations.AkimaInterpolation(chi, zed)
+#     n_chi = length(chi)
+#     k = get_clencurt_grid(kmin, kmax, N)
+#     z = get_clencurt_grid(zmin, zmax, N)
+#     z_cheb = chebpoints(n_cheb, log10(zmin), log10(zmax)) 
+#     c = FastChebInterp.ChebPoly(z_cheb, SA[log10(zmin)], SA[log10(zmax)])
+
+#     W = zeros(n_cheb+1,N) 
+#     Threads.@threads for i in 1:n_cheb+1
+#         copy_c = deepcopy(c) 
+#         copy_c.coefs .*= 0 
+#         copy_c.coefs[i] = 1.
+#         W[i,:] = copy_c.(log10.(z))
+#     end
+
+#     Bessel = zeros(n_chi, N)
+#     Threads.@threads for i in 1:n_chi
+#             Bessel[i,:] = @views SpecialFunctions.sphericalbesselj.(ℓ, chi_z(zed[i]) * k)
+#     end
+
+#     return W, Bessel
+
+# end
+
+############################
+function bessel_cheb_eval(
+    grid::IntegrationGrid,
+    ℓ::Number,
+    chi::AbstractArray,
+    zed::AbstractArray,
+    kmin::Number,
+    kmax::Number;
+    n_cheb::Int
+)
+    chi_z = DataInterpolations.AkimaInterpolation(chi, zed)
     n_chi = length(chi)
-    k = get_clencurt_grid(kmin, kmax, N)
-    z = get_clencurt_grid(zmin, zmax, N)
-#    zed = DataInterpolations.AkimaInterpolation(chi, z);
-    z_cheb = chebpoints(n_cheb, log10(zmin), log10(zmax)) 
-    c = FastChebInterp.ChebPoly(z_cheb, SA[log10(zmin)], SA[log10(zmax)])
+    k = get_clencurt_grid(kmin, kmax, length(grid.w))
+    z_cheb = chebpoints(n_cheb, log10(first(grid.z)), log10(last(grid.z)))
+    c = FastChebInterp.ChebPoly(z_cheb, SA[log10(first(grid.z))], SA[log10(last(grid.z))])
 
-    W = zeros(n_cheb+1,N) 
-    Threads.@threads for i in 1:n_cheb+1
-        copy_c = deepcopy(c) 
-        copy_c.coefs .*= 0 
-        copy_c.coefs[i] = 1.
-        W[i,:] = copy_c.(log10.(z))
+    W = zeros(n_cheb + 1, length(grid.z))
+    for i in 1:n_cheb+1
+        copy_c = deepcopy(c)
+        copy_c.coefs .= 0
+        copy_c.coefs[i] = 1.0
+        W[i, :] = copy_c.(log10.(grid.z))
     end
 
-    Bessel = zeros(n_chi, N)
+    Bessel = zeros(n_chi, length(k))
     Threads.@threads for i in 1:n_chi
-            Bessel[i,:] = @views SpecialFunctions.sphericalbesselj.(ℓ, zed[i] * k)
+        Bessel[i, :] = SpecialFunctions.sphericalbesselj.(ℓ, chi_z(zed[i]) * k)
     end
 
     return W, Bessel
-
 end
 
-function compute_W̃(ℓ::Number, chi::AbstractArray, zed::AbstractArray, kmin::Number, kmax::Number, zmin::Number, zmax::Number; n_cheb::Int = 119, N::Int = 2^(15)+1)
-    if zmin >= zmax 
-        throw(DomainError("The integration range is unphysical. Make sure zmin < zmax.")) 
-    end
-    
-    nz = length(zed)
-    n_chi = length(chi)
-    z = get_clencurt_grid(zmin, zmax, N)
-    w = get_clencurt_weights(kmin, kmax, N)
-    k = get_clencurt_grid(kmin, kmax, N)
-    kp = k
-    W, Bessel1 = bessel_cheb_eval(ℓ, chi, zed, kmin, kmax, zmin, zmax; n_cheb, N)
+# function compute_W̃(ℓ::Number, chi::AbstractArray, zed::AbstractArray, kmin::Number, kmax::Number, zmin::Number, zmax::Number; n_cheb::Int = 119, N::Int = 2^(15)+1)
+#     if zmin >= zmax 
+#         throw(DomainError("The integration range is unphysical. Make sure zmin < zmax.")) 
+#     end
 
-    W_tilde = zeros(1, nz, nz, n_cheb+1)
+#     nz = length(zed)
+#     n_chi = length(chi)
+#     chi_z = DataInterpolations.AkimaInterpolation(chi, zed)
+#     z = get_clencurt_grid(zmin, zmax, N)
+#     w = get_clencurt_weights(kmin, kmax, N)
+#     k = get_clencurt_grid(kmin, kmax, N)
+#     kp = k
+#     W, Bessel1 = bessel_cheb_eval(ℓ, chi, zed, kmin, kmax, zmin, zmax; n_cheb, N)
+
+#     W_tilde = zeros(1, nz, nz, n_cheb+1)
     
-    for (p, q) in enumerate(chi)
-        Bessel2 = zeros(n_chi, N)
+#     for (p, q) in enumerate(chi)
+#         Bessel2 = zeros(n_chi, N)
+        
+#         Threads.@threads for i in 1:n_chi
+#             Bessel2[i,:] = @views SpecialFunctions.sphericalbesselj.(ℓ, chi_z(zed[i]) * kp)
+#         end
+
+#         α = w
+         
+#         @tturbo for l in 1:n_cheb+1, i in 1:nz
+#             Cij = zero(eltype(w))
+#             for k in 1:N
+#                 Cij +=  W[l,k] * Bessel1[i,k] * Bessel2[i,k] * α[k]
+#             end
+#             W_tilde[1,i,p,l] = Cij
+#         end
+#     end
+
+#     return W_tilde
+
+# end
+
+function compute_W̃(
+    grid::IntegrationGrid,
+    ℓ::Number,
+    chi::AbstractRange,
+    zed::AbstractArray,
+    kmin::Number,
+    kmax::Number;
+    n_cheb::Int = 119
+)
+    if first(grid.z) <= last(grid.z)
+        throw(DomainError("The integration range is unphysical. Make sure zmin < zmax."))
+    end
+
+    n_z = length(zed)
+    n_chi = length(chi)
+    chi_z = DataInterpolations.AkimaInterpolation(chi, zed)
+    k = get_clencurt_grid(kmin, kmax, length(grid.w))
+    kp = k
+    W, Bessel1 = bessel_cheb_eval(grid, ℓ, chi, zed, kmin, kmax; n_cheb)
+    W_tilde = zeros(1, n_z, n_z, n_cheb+1)
+    
+    for (p, q) in enumerate(zed)
+        Bessel2 = zeros(n_chi, length(grid.w))
         
         Threads.@threads for i in 1:n_chi
-            Bessel2[i,:] = @views SpecialFunctions.sphericalbesselj.(ℓ, zed[i] * kp)
+            Bessel2[i,:] = @views SpecialFunctions.sphericalbesselj.(ℓ, chi_z(zed[i]) * kp)
         end
 
-        α = w
+        α = grid.w
          
-        @tturbo for l in 1:n_cheb+1, i in 1:nz
+        @tturbo for l in 1:n_cheb+1, i in 1:n_z
             Cij = zero(eltype(w))
-            for k in 1:N
+            for k in 1:length(grid.w)
                 Cij +=  W[l,k] * Bessel1[i,k] * Bessel2[i,k] * α[k]
             end
             W_tilde[1,i,p,l] = Cij
@@ -61,31 +153,72 @@ function compute_W̃(ℓ::Number, chi::AbstractArray, zed::AbstractArray, kmin::
 
 end
 
+# function compute_W̃_chebcoeffs(
+#     chi_interp,       # interpolatore χ(z)
+#     n_z,              # funzione n(z)
+#     b_z,              # funzione b(z) (bias)
+#     D_z,              # funzione D(z) (growth factor)
+#     #H_z,              # funzione H(z)
+#     zmin::Number,
+#     zmax::Number;
+#     n_cheb::Int,
+#     N::Int = 2^15 + 1
+# )
+#     # Stessi nodi e pesi usati in compute_W_tilde_modes
+#     CC_obj = FastTransforms.chebyshevmoments1(Float64, N)
+#     t_cc   = FastTransforms.clenshawcurtisnodes(Float64, N)
+#     w_cc   = FastTransforms.clenshawcurtisweights(CC_obj)
+
+#     z_cc = @. (zmax + zmin) / 2 + (zmax - zmin) / 2 * t_cc
+#     χ_cc = chi_interp.(z_cc)
+#     jac  = (zmax - zmin) / 2
+
+#     # Valuta W_tilde su tutti i nodi CC
+#     #W_vals = @. χ_cc^2 * n_z(z_cc) * b_z(z_cc) * D_z(z_cc) * H_z(z_cc)
+#     W_vals = @. χ_cc^2 * n_z(z_cc) * b_z(z_cc) * D_z(z_cc)
+
+#     # Costruisci T_mat: stessa logica di compute_W_tilde_modes
+#     c_basis = FastChebInterp.ChebPoly(zeros(n_cheb+1), SA[-1.0], SA[1.0])
+#     T_mat = zeros(n_cheb+1, N)
+#     Threads.@threads for n in 1:n_cheb+1
+#         basis = deepcopy(c_basis)
+#         basis.coefs[n] = 1.0
+#         T_mat[n, :] = basis.(t_cc)
+#     end
+
+#     # Proiezione: c_n = jac * sum_t w_cc[t] * W_vals[t] * T_n(t_cc[t])
+#     c_n = zeros(n_cheb+1)
+#     @tturbo for n in 1:n_cheb+1
+#         s = zero(Float64)
+#         for t in 1:N
+#             s += w_cc[t] * W_vals[t] * T_mat[n, t]
+#         end
+#         c_n[n] = jac * s
+#     end
+
+#     return c_n  # vettore [n_cheb]
+# end
+##################
 function compute_W̃_chebcoeffs(
-    chi_interp,       # interpolatore χ(z)
-    n_z,              # funzione n(z)
-    b_z,              # funzione b(z) (bias)
-    D_z,              # funzione D(z) (growth factor)
-    #H_z,              # funzione H(z)
-    zmin::Number,
-    zmax::Number;
-    n_cheb::Int,
-    N::Int = 2^15 + 1
+    grid::IntegrationGrid,
+    chi_interp,
+    n_z,
+    b_z,
+    D_z;
+    n_cheb::Int = 119
 )
-    # Stessi nodi e pesi usati in compute_W_tilde_modes
-    CC_obj = FastTransforms.chebyshevmoments1(Float64, N)
-    t_cc   = FastTransforms.clenshawcurtisnodes(Float64, N)
-    w_cc   = FastTransforms.clenshawcurtisweights(CC_obj)
+    N = length(grid.z)
+    z_cc = grid.z
+    w_cc = grid.w
+    jac  = grid.jac
 
-    z_cc = @. (zmax + zmin) / 2 + (zmax - zmin) / 2 * t_cc
+    center = (first(z_cc) + last(z_cc)) / 2
+    t_cc = @. (z_cc - center) / jac
+
     χ_cc = chi_interp.(z_cc)
-    jac  = (zmax - zmin) / 2
 
-    # Valuta W_tilde su tutti i nodi CC
-    #W_vals = @. χ_cc^2 * n_z(z_cc) * b_z(z_cc) * D_z(z_cc) * H_z(z_cc)
     W_vals = @. χ_cc^2 * n_z(z_cc) * b_z(z_cc) * D_z(z_cc)
 
-    # Costruisci T_mat: stessa logica di compute_W_tilde_modes
     c_basis = FastChebInterp.ChebPoly(zeros(n_cheb+1), SA[-1.0], SA[1.0])
     T_mat = zeros(n_cheb+1, N)
     Threads.@threads for n in 1:n_cheb+1
@@ -94,7 +227,6 @@ function compute_W̃_chebcoeffs(
         T_mat[n, :] = basis.(t_cc)
     end
 
-    # Proiezione: c_n = jac * sum_t w_cc[t] * W_vals[t] * T_n(t_cc[t])
     c_n = zeros(n_cheb+1)
     @tturbo for n in 1:n_cheb+1
         s = zero(Float64)
@@ -104,26 +236,70 @@ function compute_W̃_chebcoeffs(
         c_n[n] = jac * s
     end
 
-    return c_n  # vettore [n_cheb]
+    return c_n
 end
 
-function compute_Sℓ(
-    I_A::AbstractArray,
-    I_B::AbstractArray,
-    k_array::AbstractVector;
-    Pk::AbstractVector = ones(length(k_array)),
-    normalization::Real = 2/pi
-)
-    nk = length(k_array)
+# function compute_W̃_chebcoeffs(
+#     chi_interp,       # interpolatore χ(z)
+#     n_z,              # funzione n(z)
+#     b_z,              # funzione b(z) (bias)
+#     D_z,              # funzione D(z) (growth factor)
+#     #H_z,              # funzione H(z)
+#     zmin::Number,
+#     zmax::Number;
+#     n_cheb::Int,
+#     N::Int = 2^15 + 1
+# )
+#     # Stessi nodi e pesi usati in compute_W_tilde_modes
+#     CC_obj = FastTransforms.chebyshevmoments1(Float64, N)
+#     t_cc   = FastTransforms.clenshawcurtisnodes(Float64, N)
+#     w_cc   = FastTransforms.clenshawcurtisweights(CC_obj)
 
-    w_k = Blast.simpson_weight_array(nk)
-    Δk = nk > 1 ? (last(k_array) - first(k_array)) / (nk - 1) : one(eltype(k_array))
+#     z_cc = @. (zmax + zmin) / 2 + (zmax - zmin) / 2 * t_cc
+#     χ_cc = chi_interp.(z_cc)
+#     jac  = (zmax - zmin) / 2
 
-    integrand_k = normalization .* k_array.^2 .* Pk .* w_k .* Δk
+#     # Valuta W_tilde su tutti i nodi CC
+#     #W_vals = @. χ_cc^2 * n_z(z_cc) * b_z(z_cc) * D_z(z_cc) * H_z(z_cc)
+#     W_vals = @. χ_cc^2 * n_z(z_cc) * b_z(z_cc) * D_z(z_cc)
 
-    @tullio S[li, ik1, ik2] := integrand_k[m] *
-        I_A[li, ik1, m] *
-        I_B[li, ik2, m]
+#     # Costruisci T_mat: stessa logica di compute_W_tilde_modes
+#     c_basis = FastChebInterp.ChebPoly(zeros(n_cheb+1), SA[-1.0], SA[1.0])
+#     T_mat = zeros(n_cheb+1, N)
+#     Threads.@threads for n in 1:n_cheb+1
+#         basis = deepcopy(c_basis)
+#         basis.coefs[n] = 1.0
+#         T_mat[n, :] = basis.(t_cc)
+#     end# # Proiezione: c_n = jac * sum_t w_cc[t] * W_vals[t] * T_n(t_cc[t])
+#     c_n = zeros(n_cheb+1)
+#     @tturbo for n in 1:n_cheb+1
+#         s = zero(Float64)
+#         for t in 1:N
+#             s += w_cc[t] * W_vals[t] * T_mat[n, t]
+#         end
+#         c_n[n] = jac * s
+#     end
 
-    return S
-end
+#     return c_n  # vettore [n_cheb]
+# end
+
+# function compute_Sℓ(
+#     I_A::AbstractArray,
+#     I_B::AbstractArray,
+#     k_array::AbstractVector;
+#     Pk::AbstractVector = ones(length(k_array)),
+#     normalization::Real = 2/pi
+# )
+#     nk = length(k_array)
+
+#     w_k = Blast.simpson_weight_array(nk)
+#     Δk = nk > 1 ? (last(k_array) - first(k_array)) / (nk - 1) : one(eltype(k_array))
+
+#     integrand_k = normalization .* k_array.^2 .* Pk .* w_k .* Δk
+
+#     @tullio S[li, ik1, ik2] := integrand_k[m] *
+#         I_A[li, ik1, m] *
+#         I_B[li, ik2, m]
+
+#     return S
+# end
