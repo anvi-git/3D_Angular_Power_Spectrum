337c406a650f5ae68e82cc4a9a4a18435c80a035
anvi-git <antonio.viscusi@edu.unito.it>
2026-06-29 17:28:55 +0200

    new results

--- notebooks/blast_code/src/new_new_funcs.jl
+++ notebooks/blast_code/src/new_new_funcs.jl
@@ -37,39 +37,24 @@
 # end
 
 function W_tilde_computed(ℓ::Number, zmin::Number, zmax::Number, kmin::Number, kmax::Number,
-                             z_range::AbstractArray, n_cheb::Int, N::Number, chi_of_z::Any)
+                             Nk::Int, Nkp::Int, n_cheb::Int, N::Number, chi_of_z::Any)
 
     if zmin >= zmax
         throw(DomainError("The integration range is unphysical. Make sure zmin < zmax."))
     end
 
-    Nz = length(z_range)
-    Nk = Nz
-    Nkp = Nz - 20
-
     kp = get_clencurt_grid_z(kmin, kmax, Nkp)
-    k  = get_clencurt_grid_z(kmin, kmax, Nk)
-    z  = get_clencurt_grid_z(zmin, zmax, N)
     w  = get_clencurt_weights_z(zmin, zmax, N)
 
-    T, Bessel1 = bessel_func(ℓ, zmin, zmax, kmin, kmax, z_range, n_cheb, N, chi_of_z)
+    T, Bessel1 = bessel_func(ℓ, zmin, zmax, kmin, kmax, Nk, n_cheb, N, chi_of_z)
     T_tilde = zeros(eltype(w), Nk, Nkp, n_cheb + 1, 1)
-    
-    Bessel2 = zeros(Nkp, N)
-    chi_vals = chi_of_z.(z)
-
-    Threads.@threads for j in 1:Nkp
-        kj = kp[j]
-        for i in 1:N
-            @inbounds Bessel2[j, i] = sphericalbesselj(ℓ, chi_vals[i] * kj)
-        end
-    end
+    _, Bessel2 = bessel_func(ℓ, zmin, zmax, kmin, kmax, Nkp, n_cheb, N, chi_of_z)
 
     for ic in 1:(n_cheb + 1) 
         @tturbo for ik in 1:Nk, ikp in 1:Nkp
             Cij = zero(eltype(w))
             for iN in 1:N
-                Cij += T[ic, iN] * Bessel1[ik, iN] * Bessel2[ikp, iN] * w[iN]
+                Cij += T[ic, iN] * Bessel1[ik, iN] * Bessel2[ikp, iN] * w[iN] * kp[ikp]
             end
         T_tilde[ik, ikp, ic, 1] = Cij
         end
@@ -79,9 +64,8 @@ function W_tilde_computed(ℓ::Number, zmin::Number, zmax::Number, kmin::Number,
 end
 
 function bessel_func(ℓ::Number, zmin::Number, zmax::Number, kmin::Number, kmax::Number, 
-                                 z_range::AbstractArray, n_cheb::Int, N::Integer, chi_of_z::Any)
-    Nz = length(z_range)
-    Nk = Nz
+                                 Nk::Int, n_cheb::Int, N::Integer, chi_of_z::Any)
+    
     k = get_clencurt_grid_z(kmin, kmax, Nk)
     z = get_clencurt_grid_z(zmin, zmax, N)
 
@@ -101,7 +85,9 @@ function bessel_func(ℓ::Number, zmin::Number, zmax::Number, kmin::Number, kmax
     Threads.@threads for j in 1:Nk
         kj = k[j]
         for i in 1:N
-            @inbounds Bessel[j, i] = sphericalbesselj(ℓ, chi_vals[i] * kj)
+#           Bessel[i,:] = @views SpecialFunctions.sphericalbesselj.(ℓ, χhi[i] * x)
+            Bessel[j, i] = @views SpecialFunctions.sphericalbesselj.(ℓ, chi_vals[i] * kj)
+            #@inbounds Bessel[j, i] = sphericalbesselj(ℓ, chi_vals[i] * kj)
         end
     end
 
