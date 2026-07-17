31d6b3a08d06d53415a45e4498ddf1c59765038e
anvi-git <antonio.viscusi@edu.unito.it>
2026-07-17 17:15:03 +0200

    new results

--- notebooks/blast_code/src/config.jl
+++ notebooks/blast_code/src/config.jl
@@ -7,10 +7,10 @@ using DataInterpolations
 function setup_cosmology_grid(;
     cosmo  = Blast.FlatΛCDM(),
     N      = 2^15 + 1,
-    Nk     = 150,
-    Nkp    = 150,
-    Nkpp   = 150,
-    n_cheb = 200
+    Nk     = 350,
+    Nkp    = 350,
+    Nkpp   = 350,
+    n_cheb = 50
 )
     # background quantities
     z_b      = npzread("blast_code/data/background/z.npy")
